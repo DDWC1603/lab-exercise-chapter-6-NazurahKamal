381a5c07b5f36a2b9531c175b340bd6ee7bd4b9a
NazurahKamal <33883971+NazurahKamal@users.noreply.github.com>
2018-01-22 10:34:18 +0800

    task complete

--- String-Exercise-1.cpp
+++ String-Exercise-1.cpp
@@ -2,14 +2,26 @@
 //display the string below using the string function state below.
 
 #include <iostream>
+#include<cstring>
+using namespace std;
 
 int main()
 {
-	char engkau[10];
-
-	//stringcopy(a,"hello");
+	char engkau1[10]="a";
+	char engkau2[10]="hello";
+	char engkau3[10];
+	int len;
 	
-	cout<<engkau;
-
+	//stringcopy(a,"hello");
+	strcpy(engkau3, engkau1);
+	cout<<"strcpy(engkau1, engkau2, engkau3): "<<engkau3 <<endl ;
+	
+	strcat(engkau1, engkau2);
+	cout<<"strcat(engkau1, engkau2): " <<engkau1 <<endl;
+	
+	len=strlen(engkau1);
+	cout<<"strlen(engkau1) : " <<len <<endl;
+	
 	return 0;
 }
+
